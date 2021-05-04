f51ae8f1fc1a3c881073403aaa82c65fd1b90039
freakeinstein <wow.dr.dre@gmail.com>
2021-05-04 22:58:07 +0530

    communicate to X website

--- chrome/background.js
+++ chrome/background.js
@@ -7,14 +7,44 @@ chrome.runtime.onInstalled.addListener(function(details) {
         chrome.tabs.create({ url: "https://aquila.network" });
     }
 
-    chrome.storage.sync.set({axapi: { "host": "http://localhost:5000/"}});
+    chrome.storage.sync.set({axapi: { "host": "http://localhost:5000", "isURL": true }});
+});
+
+// This event is fired with the user accepts the input in the omnibox.
+chrome.omnibox.onInputEntered.addListener((text) => {
+    // Encode user input for special characters , / ? : @ & = + $ #
+    chrome.storage.sync.get("axapi", function(data) {
+        var newURL = "https://aquila.network/x" + "?q=" + encodeURIComponent(text);
+        if (data.axapi.isURL) {
+            newURL = data.axapi.host + "?q=" + encodeURIComponent(text);
+        }
+        chrome.tabs.update({ url: newURL });
+    });
+});
+
+chrome.runtime.onMessageExternal.addListener(
+    function(request, sender, sendResponse) {
+        chrome.storage.sync.get("axapi", function(data) {
+            if (data.axapi.isURL) {
+                sendResponse({
+                    message: "No API key",
+                    success: false
+                });
+            } else {
+                sendResponse({
+                    key: data.axapi.host,
+                    message: "Found API key",
+                    success: true
+                });
+            }
+        });
 });
 
 chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
             pageUrl: {
-                schemes: ['https']
+                schemes: ['http', 'https']
             },
         })],
         actions: [new chrome.declarativeContent.ShowPageAction()]
--- chrome/popup.js
+++ chrome/popup.js
@@ -8,9 +8,19 @@ window.onload = function() {
     });
 };
 
+function validURL(str) {
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+        '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return !!pattern.test(str);
+}
+
 indexPage.onclick = function() {
     // update api url
-    chrome.storage.sync.set({axapi: { "host": hostInput.value }});
+    chrome.storage.sync.set({axapi: { "host": hostInput.value, "isURL": validURL(hostInput.value) }});
 
     apiError.innerHTML = "Processing.."
     // get page html
