9e9d923821b3b5669a29dfd44698a4d89adb6cdf
freakeinstein <wow.dr.dre@gmail.com>
2021-11-17 20:42:51 +0530

    changes to support localhost

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# AquilaX-browser-extension
-Browser extension to access Aquila X
+<div align="center">
+  <a href="https://aquila.network">
+    <img
+      src="https://user-images.githubusercontent.com/19545678/133918727-5a37c6be-676f-427b-8c86-dd50f58d1287.png"
+      alt="Aquila Network Logo"
+      height="64"
+    />
+  </a>
+  <br />
+  <p>
+    <h3>
+      <b>
+        Aquila X browser extensions
+      </b>
+    </h3>
+  </p>
+  <p>
+    <b>
+      Browser extensions to augment Aquila X UI
+    </b>
+  </p>
+  <br/>
+</div>
+
+Aquila X Browser Extensions are used to connect with Aquila X backend from a web browser. Here is a bird's eye view of where Aquila X browser extensions fit in the entire ecosystem:
+<div align="center">
+  <img
+    src="https://user-images.githubusercontent.com/19545678/133918433-5c794a56-ea8f-4566-b280-9c3058ccd67c.png"
+    alt="Aquila X Browser extensions"
+    height="400"
+  />
+ <br/>
+</div>
+
+### Install from web store
+[<img height=46px src="https://user-images.githubusercontent.com/68724239/111738541-92476300-88a7-11eb-8444-3f2baa515b9c.png"/>](https://chrome.google.com/webstore/detail/aquila-x/albdahjdcmldbcpjmbnbcbckgndaibnk)
+[<img height=46px src="https://user-images.githubusercontent.com/19545678/137582002-85df2d6f-2ad5-43ca-a673-04f4925b8c41.png"/>](https://addons.mozilla.org/en-US/firefox/addon/aquilax/)
--- firefox/background.js
+++ firefox/background.js
@@ -1,31 +1,31 @@
 const aquilaUrl = 'https://x.aquila.network';
 const defaultHost = 'http://localhost';
 
-chrome.runtime.onInstalled.addListener(function(details) {
+browser.runtime.onInstalled.addListener(function(details) {
     if(details.reason == "install"){
         //handle a first install
-        chrome.tabs.create({ url: `${aquilaUrl}/next.html` });
+        browser.tabs.create({ url: `${aquilaUrl}/next.html` });
     }else if(details.reason == "update"){
         //handle an update
-        chrome.tabs.create({ url: `${aquilaUrl}/updates.html` });
+        browser.tabs.create({ url: `${aquilaUrl}/updates.html` });
     }
-    chrome.storage.sync.set({axapi: { host: defaultHost, isURL: true }});
+    browser.storage.sync.set({axapi: { host: defaultHost, isURL: true }});
 });
 
 // This event is fired with the user accepts the input in the omnibox.
-chrome.omnibox.onInputEntered.addListener((text) => {
+browser.omnibox.onInputEntered.addListener((text) => {
     // Encode user input for special characters , / ? : @ & = + $ #
-    chrome.storage.sync.get("axapi", function(data) {
+    browser.storage.sync.get("axapi", function(data) {
         let newURL = `${aquilaUrl}/?q=${encodeURIComponent(text)}`;
         if (data.axapi.isURL) {
             newURL = `${data.axapi.host}?q=${encodeURIComponent(text)}`;
         }
-        chrome.tabs.update({ url: newURL });
+        browser.tabs.update({ url: newURL });
     });
 });
 
-chrome.runtime.onMessageExternal.addListener(
-    function(request, sender, sendResponse) {
+browser.runtime.onMessageExternal.addListener(
+    function(message, sender, sendResponse) {
         chrome.storage.sync.get("axapi", function(data) {
             if (data.axapi.isURL) {
                 sendResponse({
@@ -40,15 +40,4 @@ chrome.runtime.onMessageExternal.addListener(
                 });
             }
         });
-});
-
-chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {
-                schemes: ['http', 'https']
-            },
-        })],
-        actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
 });
